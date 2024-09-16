75c656b3f15dc5d927440957049d56a5e0a8b5f5
samandeep007 <hacktechnicals@gmail.com>
2024-09-15 22:48:46 -0600

    The size of integers and their ambiguities

--- integers.cpp
+++ integers.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
+#include <iostream> //it is a library
 #include <string>
+#include <cstdint> // c standard int
 
 using namespace std;
 
 int main() {
 	//1 byte = 8 bits --> short int -->  int --> long int --> long long int
-	printf("Size of this data type is %ld bits\n", sizeof(short int) * 8);
-	printf("Size of this data type is %ld bits\n", sizeof(int) * 8);
-	printf("Size of this data type is %lld bits\n", sizeof(long int) * 8);
-	printf("Size of this data type is %lld bits\n", sizeof(long long) * 8);
+	printf("Size of this data type is %ld bits\n", sizeof(int16_t) * 8);
+	printf("Size of this data type is %ld bits\n", sizeof(int32_t) * 8);
+	printf("Size of this data type is %lld bits\n", sizeof(int64_t) * 8);
+	//printf("Size of this data type is %lld bits\n", sizeof(int128_t) * 8);
+
+	printf("The size of this data type is %ld bits\n", sizeof(char) * 8);
+	printf("The size of this data type is %ld bits\n", sizeof(string) * 8);
+	printf("The size of this data type is %ld bits\n", sizeof(bool) * 8);
+
+	//int fun = 0x16; //It converts hexadecimal to decimal
+	int fun = 0b00010110;
+	printf("Fun value is %d\n", fun);
 	return 0;
 }
