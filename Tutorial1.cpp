cb29424da43702fbd240a254161655cff2ca5a3b
samandeep007 <hacktechnicals@gmail.com>
2024-09-15 18:01:09 -0600

    Understanding the anatomy of a C++ program

--- Tutorial1.cpp
+++ Tutorial1.cpp
@@ -20,15 +20,15 @@ static char iamachar() {
 	return 'S';
 }
 
-int main() { //Entry point of the C++ prpgram
-	cout << "Enter your name: ";
-	string name;
-	cin >> name;
-	cout << "Welcome " + name + " to the first class of SODV:1101 Programming Fundamentals" << endl;
-	nothing();
-	cout << endl << iamachar();
-	return 0;
-}
+//int main() { //Entry point of the C++ prpgram
+//	cout << "Enter your name: ";
+//	string name;
+//	cin >> name;
+//	cout << "Welcome " + name + " to the first class of SODV:1101 Programming Fundamentals" << endl;
+//	nothing();
+//	cout << endl << iamachar();
+//	return 0;
+//}
 
 //C boilerplate syntax
 //#include <stdio.h>
---
+++ color.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //Header file
+//using namespace std; //I can use std:: either
+
+int main() {
+	std::cout << "Samandeep Singh Sandhu\n";
+	std::cout << "Welcome to this class" << std::endl;
+	std::cout << "Have a nice day!";
+	return 0;
+	//cout << "This will not be executed";
+}
