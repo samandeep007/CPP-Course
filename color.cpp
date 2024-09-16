c7a23cf05e122418a29aa6f7ab1249b87c02b62c
samandeep007 <hacktechnicals@gmail.com>
2024-09-15 18:17:46 -0600

    Basic color program

--- color.cpp
+++ color.cpp
@@ -1,10 +1,21 @@
 #include <iostream> //Header file
-//using namespace std; //I can use std:: either --> It means everything is coming from the std file by default.
+using namespace std; //I can use std:: either --> It means everything is coming from the std file by default.
 
 int main() {
-	std::cout << "Samandeep Singh Sandhu\n";
+	/*std::cout << "Samandeep Singh Sandhu\n";
 	std::cout << "Welcome to this class" << std::endl;
 	std::cout << "Have a nice day!";
-	return 0;
+	return 0;*/
 	//cout << "This will not be executed";
+
+	int number; //Declaration --> dataType identifer --> Identifiers are case-sensitive
+	printf("Enter a random number of your choice: ");
+	cin >> number; //Initialization
+	printf("Your ID is %d\n", number + 3); //%d is placeholder for integers
+
+	string favoriteColor; //Variable --> container to store the favoriteColor
+	printf("Enter your favorite color: ");
+	cin >> favoriteColor;
+	cout << "My favorite color is " + favoriteColor + " as well";
+	return 0;
 }
