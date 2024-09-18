6fa5003917b0c07a66b0b069231697259bad7154
samandeep007 <hacktechnicals@gmail.com>
2024-09-17 19:35:05 -0600

    strings with foreach

--- strings.cpp
+++ strings.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
-#include <string>
+//#include <string>
 
 using namespace std;
 
 int main() {
 	char my_string[] = "Samandeep Singh"; //C style strings
-	string my_name = "Tarandeep Kaur";
-	cout << my_string << endl;
-	cout << my_name << endl;
+	char my_char_string[] = {'S', 'a', 'm', 'a', 'n','d', 'e', 'e', 'p', 0}; //Every single string is terminated by a 0 
+	//string my_name = "Tarandeep Kaur";
+	
+	cout << "Take a break" << endl;
+
+	for (int i = 0; my_string[i]; i++) { // or my_string[i] != 0 
+		cout << "Character is " << my_string[i] << endl;
+	}
+
+	cout << "Take a break part 2" << endl;
+
+	for (char *cp = my_string; *cp != 0; cp++) {
+		cout << "Character is " << *cp << endl; //using pointer deref
+	}
+	cout << my_char_string << endl;
+	//cout << my_name << endl;
+
+	cout << "Take a break part 3" << endl;
+
+	for (char c : my_string) { //An extra iteration because there's a 0 at the end
+		if (c == 0) break;
+		cout << "Character is " << c << endl; //using pointer deref
+	}
 
 	return 0;
 }
