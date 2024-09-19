748bc3112bb3dc78d7e331f5ffecaa2c409571ab
samandeep007 <hacktechnicals@gmail.com>
2024-09-18 18:52:18 -0600

    float

--- floats.cpp
+++ floats.cpp
@@ -7,7 +7,12 @@ int main() {
 	// Float data types --> float, double, long double
 	printf("Float size is %ld bits\n", sizeof(long double) * 8);
 	float my_p_value = 20.2 + 20.2;
-	printf("My P value is: %.1f in float\n", my_p_value);
+
+	if (my_p_value = 40.400002) {
+		puts("You got it right\n");
+	}
+
+	printf("My P value is: %.40f in float\n", my_p_value);
 	/*cout << my_float;*/
 	return 0;
 }
