5487187ee645493b75136a3fd7e24e8e00a23d10
samandeep007 <hacktechnicals@gmail.com>
2024-09-20 23:27:56 -0600

    Heap and stack memory

--- stackAndHeap.cpp
+++ stackAndHeap.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
+struct User {
+	const int id;
+	int age;
+	User(): id(001), age(22){} //This is constructor
+	
+};
 
 int main() {
+	using namespace std; //can be inside the main method as well
+	
 	//Stack and Heap Memory
+	
+	/*
+	Stack has predefined size --> fastest
+	Heap memory can grow --> predefined as well 
+	*/
+
+	//stack
+	int score = 100;
+	User sam;
+	cout << sam.age;
+
+
+	//Heap
 
 	return 0;
 }
