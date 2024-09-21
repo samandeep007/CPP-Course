4a4fd272b915dfcf7b4d7c9061238d46e79c9861
samandeep007 <hacktechnicals@gmail.com>
2024-09-20 20:23:12 -0600

    structs in C++

---
+++ structures.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+struct User {
+	//This is kinda blueprint --> pretty much like a interface
+	const int uID;
+	const char *name;
+	const char *email;
+	int course_count;
+};
+
+int main() {
+	 //Structures in CPP
+	User userOne = {001, "Saman Sandhu", "samandeep0792@gmail.com", 12};
+	User userTwo = { 002, "Tarandeep Sandhu", "tarandeep0792@gmail.com", 24 };
+	User *u2 = &userTwo; // . can only be used with a struct, not a pointer
+	cout << userOne.email << endl;
+	cout << userTwo.name << endl;
+	userTwo.name = "Jaideep Singh"; //Damn, you are able to change it even after it being a const? This is because name is a pointer
+	cout << userTwo.name << endl;
+	cout << u2->email; // use -> instead
+	return 0;
+}
