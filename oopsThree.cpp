99f4524397ed3906d96c3032342d8ca34c719ccc
samandeep007 <hacktechnicals@gmail.com>
2024-09-22 13:27:17 -0600

    Ambiguity in constructors

--- oopsThree.cpp
+++ oopsThree.cpp
@@ -7,29 +7,62 @@ class Phone {
 	string _name = "";
 	string _os = "";
 	int _price = 0;
+	Phone(); //Default constructor is inaccessible
 
 public: 
-	Phone();
-	//Phone(const string& name, const string& os, const int& price); //Parameter constructor
-	//Phone(const Phone&); //copy constructor
+	Phone(const string& name, const string& os, const int& price); //Parameter constructor
+	Phone(const Phone&); //copy constructor
 	
 	string getName() {
 		return _name;
 	}
 
-	~Phone(); //This is a destructor
+	string getOs() {
+		return _os;
+	}
+
+	~Phone(); //This is a destructor --> Called automatically --> LIFO
 };
 
-Phone::Phone() : _name(), _os(), _price() {
-	puts("Default constructor called\n");
+Phone::Phone() : _name("iPhone 16"), _os(), _price() {
+	puts("Default constructor called");
 };
 
+Phone::Phone(const string& name, const string& os, const int& price) : _name(name), _os(os), _price(price) {
+	puts("Parameter constructor called");
+};
+
+Phone::~Phone() {
+	cout << "Destructor called for " << _name << endl;
+}
+
+Phone::Phone(const Phone& values) {
+	puts("Overwrite copy constructor called");
+	_name = "my-" + values._name;
+	_os = "skinned-"+values._os; //Modified a lil bit
+	_price = values._price;
+};
+// :: --> scope resolution operator
 
 int main() {
 	//Phone *apple = new Phone("iphone", "iOS", 12990);
 	//cout << apple->getName();
 
-	Phone samsung;
-	cout << samsung.getName() << endl;
+	//Phone apple;
+	//cout << apple.getName() << endl;
+
+	Phone *samsung = new Phone("S30 Ultra", "ios", 230);
+	cout << samsung -> getName() << endl;
+
+	Phone onePlus8("OP8", "Android-Oxy", 799);
+	cout << onePlus8.getName() << endl;
+	cout << onePlus8.getOs() << endl;
+
+	Phone onePlus9 = onePlus8;
+	cout << onePlus9.getName() << endl;
+	cout << onePlus9.getOs() << endl;
+
+
+
 	return 0;
 }
--- oopsTwo.cpp
+++ oopsTwo.cpp
@@ -3,27 +3,27 @@
 
 using namespace std;
 
-class User {
-	int _secret = 22;
-
-public:
-	string name = "default";
-	void classMessage();
-	void setSecret(const int& newSecret);
-	int getSecret() const;
-};
-
-void User::classMessage() {
-	cout << "Good morning, " << name << endl;
-}
-
-void User::setSecret(const int& newSecret) {
-	_secret = newSecret;
-}
-
-int User::getSecret() const {
-	return _secret;
-}
+//class User {
+//	int _secret = 22;
+//
+//public:
+//	string name = "default";
+//	void classMessage();
+//	void setSecret(const int& newSecret);
+//	int getSecret() const;
+//};
+//
+//void User::classMessage() {
+//	cout << "Good morning, " << name << endl;
+//}
+//
+//void User::setSecret(const int& newSecret) {
+//	_secret = newSecret;
+//}
+//
+//int User::getSecret() const {
+//	return _secret;
+//}
 
 static int oopsTwo() {
 	return 0;
