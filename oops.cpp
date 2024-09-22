02259aadea261baec60ab54557603236ab598a0f
samandeep007 <hacktechnicals@gmail.com>
2024-09-22 01:07:14 -0600

    getters and setters

--- oops.cpp
+++ oops.cpp
@@ -4,25 +4,29 @@
 using namespace std;
 
 //Class is a blueprint on which the objects are based
-class User {
-	int secret = 22;  //data members are private by default --> kitchen of restaurant --> we use getters and setters for it
+class User { // --> If a class only has variables or fields, it's a better practice to better define it as a struct
+
+	int _secret = 22;  //data members are private by default --> kitchen of restaurant --> we use getters and setters for it
+	//private members have a _ prefix, while the system variables have __
 
 public: 
 	string name = "default"; //We can overwrite this value
 	
 	//Method
-	void classMessage() {
-		cout << "Class is great, " << name << endl;
-	}
+	void classMessage(); 
 
 	int getSecret() { //This is a getter
-		return secret;
+		return _secret;
 	}
 
 	void setSecret(const int &newSecret) { //This is a setter --> You can add validation if you want
-		secret = newSecret;
+		_secret = newSecret;
 	}
 };
+
+void User::classMessage() { //We need to tell this function that it belongs to a class explicitly
+	cout << "Class is great, " << name << endl;
+}
 
 int main() {
 	User saman;
