fce100cc1d4741e21e070a88fb47d556dcddd48a
samandeep007 <hacktechnicals@gmail.com>
2024-09-23 17:44:54 -0600

    STL

--- STL1.cpp
+++ STL1.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+template<typename T>
+T getBigger(T valOne, T valTwo) { //No need to worry about the data type: T can be anything
+	return valOne > valTwo ? valOne : valTwo;
+}
 int main() {
+	//Standard Template Library --> For generic programming --> Different from std library
+	/*
+	It takes Generic programming to next level --> Somewhere between pseudocode and actual code --> data type doesn't matter
+	It provides built in data structures and algorithms
+	Ships in --> Similar to collection API in Java
+	Generic programming -> Data types are not specified at the time of implementation of code logic
+	--> Also known as run-time polymorphism
+	NOTE: we will mention data type in C++
+	*/
+
 
 	return 0;
 }
