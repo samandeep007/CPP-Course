07af3b9de8e4b06c7b4a0522ff1573731b8a9980
samandeep007 <hacktechnicals@gmail.com>
2024-09-22 21:35:10 -0600

    File handling

--- files.cpp
+++ files.cpp
@@ -4,6 +4,13 @@
 using namespace std;
 
 int main() {
-	//File Handling in C++
+	//File Handling in C++ --> doesn't work in visual studio
+	static const char * originalFile = "original.txt";
+	static const char * editedFile = "editedFile.txt";
+
+	//rename(originalFile, editedFile);
+	remove(editedFile);
+	//FILE* fh = fopen(originalFile, "w"); //r -> read, w -> write, a -> append
+	//fclose(fh);
 	return 0;
 }
