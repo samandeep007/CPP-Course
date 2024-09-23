027f921a07b8435579d26de1cc1c0340b65e6439
samandeep007 <hacktechnicals@gmail.com>
2024-09-22 20:05:57 -0600

    vector --> ArrayList

--- oopsEight.cpp
+++ oopsEight.cpp
@@ -3,24 +3,44 @@
 
 using namespace std;
 
-void swapValues(int& valOne, int& valTwo) {
+void swapValues(int& valOne, int& valTwo) { //temp is nothing but a wastage of memory --> This can be really extensive with classes with 100s of data members
 	int temp = valOne;
 	valOne = valTwo;
 	valTwo = temp;
 }
 
-int main() {
+
+//better way using move semantics
+void swap(int& valOne, int& valTwo) {
+	int temp = move(valOne); //not a reference but the value of valOne is moved to temp, valOne is empty
+	valOne = move(valTwo);
+	valTwo = move(temp);
+}
+
+// l values and r values --> Left and right values of the operator --> right value is not available in the next line
+
+string printMe() {
+	return "I am print";
+}
+
+static int moveSemantics() {
 	//Move semantics --> new concept --> Alternative to copy constructor	
 	int a = 10;
 	int b = 20;
 	
-	cout << "The initial value of a is " << a << endl;
-	cout << "The initial value of b is " << b << endl;
+	cout << "A: " << a << endl;
+	cout << "B: " << b << endl;
 
 	swap(a, b);
 	
-	cout << "The value of a is " << a << endl;
-	cout << "The value of b is " << b << endl;
+	cout << "new A: " << a << endl;
+	cout << "new B: " << b << endl;
+
+	string s = printMe(); //left value will be available in the next line while printMe() will not --> it copies the reference into s
+	string&& ss = printMe(); //difference: we are moving printMe to ss
+
+
+
 	
 	return 0;
 }
---
+++ oopsNine.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector> //similar to ArrayList
+
+using namespace std;
+
+
+
+int main() {
+	//standard template library --> Template is a empty plate on which you can put anything
+
+	return 0;
+}
