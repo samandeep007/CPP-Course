cd80d2fa4fd4627bf3a51dc2361dfcbf6a3cd846
samandeep007 <hacktechnicals@gmail.com>
2024-09-23 20:06:56 -0600

    containers --> vectors

--- STL6.cpp
+++ STL6.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-int main() {
+static int partitionLib() {
 	vector<int> myIntegers = { 1,2,3,4,5,6,7,8,9,10 };
 	for (int integer : myIntegers) {
 		cout << integer << " ";
 	}
 	cout << endl;
 
-	partition(myIntegers.begin(), myIntegers.end(), [](auto x) {return x%2==0; });
+	partition(myIntegers.begin(), myIntegers.end(), [](auto x) {return x%2==0; }); //begin(myIntegers)
 	for (int integer : myIntegers) {
 		cout << integer << " ";
 	}
---
+++ STL7.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	//Containers --> array on steroids
+	//Vectors
+	vector<int> nums;
+	vector<string> heros {"batman", "flash", "superman", "robin"};
+
+	nums.push_back(5);
+	nums.push_back(6);
+	nums.push_back(7);
+	nums.push_back(8);
+	nums.push_back(10);
+
+	for (int i = 0; i < 5; i++) {
+		int input;
+		cin >> input;
+		nums.push_back(input);
+	}
+
+	for (int num : nums) {
+		cout << num << " ";
+	}
+
+	cout << endl;
+
+	cout << nums.size() << endl << nums.capacity() << endl << nums.max_size() <<  endl;
+
+	cout << nums.empty() << endl; //Returns a boolean
+
+	for (auto i = begin(nums); i != end(nums); i++) {
+		cout << *i << " ";
+	}
+
+	cout << endl;
+
+
+
+	return 0;
+}
