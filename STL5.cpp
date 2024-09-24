83ec625da955ccbf9c9ab329522eac3cc46ef73c
samandeep007 <hacktechnicals@gmail.com>
2024-09-23 19:41:15 -0600

    search in Stl

---
+++ STL5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	//Searching
+	//Binary Search --> Divide and conquer strategy --> Behind the scenes
+	int myNumbers[6] = { 2,7,8,3,5,9 };
+	if (binary_search(arr.begin(), arr.end(), 15)) //Need to debug this --> For some reason, the function is not available
+		cout << "15 exists in vector";
+	else
+		cout << "15 does not exist";
+
+	return 0;
+}
